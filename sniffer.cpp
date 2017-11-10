53bd58988cfbbb1eaee14806615277c96e309917
dda-661-4 <32516165+dda-661-4@users.noreply.github.com>
2017-11-10 14:17:12 +0300

    Add files via upload

--- sniffer.cpp
+++ sniffer.cpp
@@ -18,41 +18,13 @@ Sniffer::Sniffer(QWidget *parent) :
     ui->setupUi(this);
 }
 
-/*Packet::Packet()
-{
-    m_data=NULL;
-}
-
-Packet::Packet(const Packet &p)
-{
-    pHeader=p.pHeader;
-
-    memcpy(p.m_data,m_data,pHeader.caplen);
-}
-//прототип конструктор копирования
-void operator=(const Packet &p)
-{
-    pHeader=p.pHeader;
-    m_data=new char [pHeader.caplen];
-    memcpy(p.m_data,m_data,pHeader.caplen);
-}
-// оператор присваивания
-// функция (селектор)
-
-Packet::~Packet()
-{
-    if (m_data!=NULL);
-    delete []m_data;
-}
-*/
 Sniffer::~Sniffer()
 {
-
     delete ui;
 }
 
 int z;
- char *m_data;
+
 
 void Sniffer::on_actionExit_triggered()
 {
@@ -68,7 +40,6 @@ void Sniffer::on_open_clicked()
 {
   QString fName = QFileDialog::getOpenFileName(this,"open the file");
   QFile file(fName);
-  QFile fileOut("/home/dribl/d.txt");
   PacketStream ps;
   header pk;
 
@@ -80,19 +51,8 @@ void Sniffer::on_open_clicked()
   {
   file.read((char *)&ps.fHeader,24);
 
-  int p=file.size();
-  int allpackets=0;
-  if(fileOut.open(QIODevice::WriteOnly | QIODevice::Text))
-  {
-   /*QTextStream writeStream(&fileOut);
-    writeStream <<"linktype\t"<< ps.fHeader.linktype<<endl;
-    writeStream <<"max lenth bytes\t"<< ps.fHeader.snaplen<<endl;
-    writeStream <<"sigfigs\t"<< ps.fHeader.sigfigs<<endl;
-    writeStream <<"thiszone\t"<< ps.fHeader.thiszone<<endl;
-    writeStream <<"major\t"<< ps.fHeader.version_major<<endl;
-    writeStream <<"minor\t"<< ps.fHeader.version_minor<<endl;
-    writeStream <<"magic\t"<< ps.fHeader.magic<<endl;
-    writeStream <<"\n\n\n";*/
+    int p=file.size();
+    int allpackets=0;
 
     ui->textEdit->append("linktype\t" + QString::number(ps.fHeader.linktype));
     ui->textEdit->append("max lenth bytes\t" + QString::number(ps.fHeader.snaplen)) ;
@@ -106,40 +66,35 @@ void Sniffer::on_open_clicked()
 
     int min=65535;
     int max=0;
-
-     //ui->textBrowser->append("packets # ");
+    char *m_data;
 
     while(file.pos()<p)
    {
      allpackets++;
      file.read((char*)&pk.pHeader,16);
-     //qDebug()<<pk.pHeader.caplen;
+     qDebug()<<file.pos();
      m_data=new char [pk.pHeader.len];
-      //qDebug()<<file.pos();
+
      file.read(m_data,pk.pHeader.len);
 
-    ui->textEdit->append("packets # " + QString::number(allpackets));
+     ui->textEdit->append("packets # " + QString::number(allpackets));
      ui->textEdit->append("t2\1" + QString::number(pk.pHeader.t1)) ;
      ui->textEdit->append("t2\2" + QString::number(pk.pHeader.t2)) ;
 
-    /* writeStream << "packets # "<<allpackets<<endl;
-     writeStream << "t1\t"<<pk.pHeader.t1<<endl;
-     writeStream<< "t2\t"<< pk.pHeader.t2<<endl;*/
 
      for(int i = 0;i < pk.pHeader.len; i++)
     {
-    ui->textEdit->append("DATA\t"+QString::number(m_data[i]&0xff));
-    /*writeStream <<"DATA\t"<<hex<<(m_data[i]&0xff)<<endl;
-     writeStream <<"DATA\t"<<m_data<<endl;*/
+         QString d;
+         d=QString::number(m_data[i]);
+         int q=d.toInt();
+         QString s=QString::number(q,16).toUpper();
+         ui->pack->insertPlainText(" "+ s);
+       // ui->pack->append(QString::number(m_data[i]&0xff));
     }
 
      ui->textEdit->append("packet : bytes\n" + QString::number(pk.pHeader.len));
      ui->textEdit->append("packet : bytes\n" + QString::number(pk.pHeader.caplen)) ;
      ui->textEdit->append("\n\n");
-
-    /*writeStream <<"packet : bytes\n"<<pk.pHeader.len<<endl;
-     writeStream <<"packet : bytes\n"<<pk.pHeader.caplen<<endl;
-     writeStream <<"\n\n";*/
 
     if(pk.pHeader.caplen>max)
      {
@@ -150,7 +105,7 @@ void Sniffer::on_open_clicked()
          min=pk.pHeader.caplen;
      }
 
-    //file.seek(file.pos()+pk.pHeader.len);
+
      if (m_data!=NULL)
      {
      delete []m_data;
@@ -159,21 +114,15 @@ void Sniffer::on_open_clicked()
      ui->min->setText(QString::number(min));
      ui->max->setText(QString::number(max));
 
-     /*QFile File("/home/dribl/d.txt");
-    if((File.exists())&&(File.open(QIODevice::ReadOnly)))
-     {
-       ui->textBrowser->setText(File.readAll());
-     }*/
   }
 
-  file.close();
  }
 
-}
 
 void Sniffer::on_pushButton_2_clicked()
 {
      ui->textEdit->setText("");
      ui->max->setText("");
      ui->min->setText("");
+     ui->pack->setText("");
 }
